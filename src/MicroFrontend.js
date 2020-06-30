07e2f104a18f1c31b68bccea6248a6e14b90b3f6
Jennifer Fu <jennifer.fu@hpe.com>
2020-06-29 19:35:52 -0700

    Updated MicroFrontend.js

--- src/MicroFrontend.js
+++ src/MicroFrontend.js
@@ -4,7 +4,8 @@ const MicroFrontend = ({ name, host, history }) => {
   useEffect(() => {
     const scriptId = `micro-frontend-script-${name}`;
     const renderMicroFrontend = () => {
-      window[`render${name}`] && window[`render${name}`](`${name}-container`, history);
+      window[`render${name}`] &&
+        window[`render${name}`](`${name}-container`, history);
     };
 
     if (document.getElementById(scriptId)) {
@@ -15,27 +16,28 @@ const MicroFrontend = ({ name, host, history }) => {
     fetch(`${host}/asset-manifest.json`)
       .then(res => res.json())
       .then(manifest => {
-        let chunkCount = -1;
-        Object.keys(manifest['files'])
+        const promises = Object.keys(manifest['files'])
           .filter(key => key.endsWith('.js'))
-          .forEach((key, _, arr) => {
-            if (chunkCount < 0) {
-              chunkCount = arr.length;
-            }
-            const path = `${host}${manifest['files'][key]}`;
-            const script = document.createElement('script');
-            if (key === 'main.js') {
-              script.id = scriptId;
-            }
-            script.onload = () => {
-              chunkCount--;
-              if (chunkCount === 0) {
-                renderMicroFrontend();
-              }
-            }
-            script.src = path;
-            document.head.appendChild(script);
-          });
+          .reduce((sum, key) => {
+            sum.push(
+              new Promise(resolve => {
+                const path = `${host}${manifest['files'][key]}`;
+                const script = document.createElement('script');
+                if (key === 'main.js') {
+                  script.id = scriptId;
+                }
+                script.onload = () => {
+                  resolve();
+                };
+                script.src = path;
+                document.head.appendChild(script);
+              })
+            );
+            return sum;
+          }, []);
+        Promise.allSettled(promises).then(() => {
+          renderMicroFrontend();
+        });
       });
 
     return () => {
@@ -44,6 +46,6 @@ const MicroFrontend = ({ name, host, history }) => {
   }, [name, host, history]);
 
   return <main id={`${name}-container`} />;
-}
+};
 
 export default MicroFrontend;
